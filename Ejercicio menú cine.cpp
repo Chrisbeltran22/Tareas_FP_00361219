2b16eb3b457ccbe64968d7f2e34de2351fc5b6cb
Chris Beltrán <chrisgonza0@gmail.com>
2019-11-08 22:12:37 -0600

    Se agrega menu 3 o menu tarea en c++

---
+++ Ejercicio menú cine.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int menu=6;
+    int pelicula;
+    int comida;
+    int plato;
+    
+    while (menu != 0){ //Se inicializa que while funcionará al ser diferente de 0
+        cout<<endl<<"Selecciona una de nuestras clasificaciones de peliulas"<<endl;
+        cout<<"1- Clasificacion A"<<endl<<"2- Clasificacion B"<<endl<<"3- Clasificacion C"<<endl<<"4- Clasificacion D"<<endl<<"5- Clasificacion E"<<endl<<"0- Salir\n";
+        cin>>menu;
+            
+        if (menu == 1){ //Si menu se selecciona a 1, se despliega la opción Desayunos.
+            cout<<endl<<"Has pulsado Clasificacion A...\nLas peliculas disponibles para el dia de hoy son:";
+            cout<<endl<<"1- Frozen 2\n2- El Rey Leon"<<endl;
+            cin>>pelicula;
+            if (pelicula==1){
+                cout<<"Esperemos disfrute de Frozen"<<endl;
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                
+            }
+            
+            if (pelicula==2){
+                cout<<"Esperemos disfrute de El Rey Leon";
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                    
+            }
+        } 
+		
+		if (menu == 2){ //Si menu se selecciona a 1, se despliega la opción Desayunos.
+            cout<<endl<<"Has pulsado Clasificacion A...\nLas peliculas disponibles para el dia de hoy son:";
+            cout<<endl<<"1- Titanic"<<endl<<"2- The Bucket List"<<endl;
+            cin>>pelicula;
+            if (pelicula==1){
+                cout<<"Esperemos disfrute de Titanic"<<endl;
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                
+            }
+            
+            if (pelicula==2){
+                cout<<"Esperemos disfrute de The Bucket List";
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                    
+            }
+        }     
+        
+        if (menu == 3){ //Si menu se selecciona a 1, se despliega la opción Desayunos.
+            cout<<endl<<"Has pulsado Clasificacion A...\nLas peliculas disponibles para el dia de hoy son:";
+            cout<<endl<<"1- Marley y Yo"<<endl<<"2- Jurasic world 2"<<endl;
+            cin>>pelicula;
+            if (pelicula==1){
+                cout<<"Esperemos disfrute de Marley y Yo"<<endl;
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                
+            }
+            
+            if (pelicula==2){
+                cout<<"Esperemos disfrute de Jurasic world";
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                    
+            }
+        }
+        
+        if (menu == 4){ //Si menu se selecciona a 1, se despliega la opción Desayunos.
+            cout<<endl<<"Has pulsado Clasificacion A...\nLas peliculas disponibles para el dia de hoy son:";
+            cout<<endl<<"1- Deadpool 2"<<endl<<"2- El Guason"<<endl;
+            cin>>pelicula;
+            if (pelicula==1){
+                cout<<"Esperemos disfrute de Deadpool 2"<<endl;
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                
+            }
+            
+            if (pelicula==2){
+                cout<<"Esperemos disfrute de El Guason";
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                    
+            }
+        }
+        
+        if (menu == 5){ //Si menu se selecciona a 1, se despliega la opción Desayunos.
+            cout<<endl<<"Has pulsado Clasificacion A...\nLas peliculas disponibles para el dia de hoy son:";
+            cout<<endl<<"1- La noche del demonio"<<endl<<"2- the Hangover"<<endl;
+            cin>>pelicula;
+            if (pelicula==1){
+                cout<<"Esperemos disfrute de La noche del demonio"<<endl;
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                
+            }
+            
+            if (pelicula==2){
+                cout<<"Esperemos disfrute de The Hangover";
+                cout<<"¿Desea un combo para acompañar su pelicula?"<<endl<<"1- SI"<<endl<<"2- NO";
+                cin>>comida;
+                if (comida==1){
+                    cout<<endl<<"Seleccione un producto de nuestro menu"<<endl;
+                    cout<<"1- Pizza"<<endl<<"2- Palomitas (saladas o acarameladas)"<<endl;
+                    cin>>plato;
+                    if (plato==1){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                    if (plato==2){
+                        cout<<"Excelente eleccion, disfrute su pelicula";
+                    }
+                }
+                if (comida==2){
+                    cout<<"Disfrute de su pelicula";
+                }    
+                    
+            }
+        }
+        
+        if (menu==0){
+        	cout<<"Gracias por usar nuestros servicios"<<endl;
+        }
+	}
+}
