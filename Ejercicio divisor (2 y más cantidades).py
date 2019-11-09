cdfb5411a26b9217a63af18d308002750df1e907
Chris Beltrán <chrisgonza0@gmail.com>
2019-11-08 22:05:58 -0600

    Se agregan ejercicios de operaciones básicas de dos datos y de dos o más datos, todo en python

---
+++ Ejercicio divisor (2 y más cantidades).py
@@ -0,0 +1,24 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#Ejercicio divisor
+
+div1 = int(input("Dijite sus datos a operar: " ))
+div2 = int(input())
+dtotal = div1/div2
+print("El resultado es: " + str(dtotal))
+
+#-------------------------------------------------
+#Ejercicio divisor de dos datos o mas
+print("Iniciando ejercicio restador de dos o mas datos...")
+div1 = int(input("Dijite sus datos a operar: " ))
+div2 = int(input("segundo dato: "))
+div3 = int(input("tercer dato: "))
+div4 = int(input("cuarto dato: "))
+dtotal = div1/div2/div3/div4
+print("El resultado es: " + str(dtotal))
---
+++ Ejercicio sumador (2 y más cantidades).py
@@ -0,0 +1,29 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+#Ejercicio Sumador de dos cantidades
+
+div1 = int(input("Dijite sus datos a operar: " ))
+div2 = int(input())
+dtotal = div1+div2
+print("El resultado es: " + str(dtotal))
+
+#------------------------------------------------
+#Ejercicio sumador de dos cantidades o mas
+print("Iniciando ejercicio sumador 2...")
+div1 = int(input("Dijite sus datos a operar: " ))
+div2 = int(input("segundo dato: "))
+div4 = int(input("tercer dato: "))
+div3 = int(input("cuarto dato: "))
+dtotal = div1+div2+div3+div4
+print("El resultado es: " + str(dtotal))
+
+
+
+
+
