4dca5e1c46651a15d9b6edf59d6c7cec5e79d1e5
Chris Beltrán <chrisgonza0@gmail.com>
2019-11-08 22:01:33 -0600

    Se agrergan 4 ejercicios de operaciones básicas de dos dígitos

---
+++ Ejercicio multiplicador.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main (){
+    std::cout<<"Programa multiplicador \n";
+    int num1; int num2; int resultado;
+    std::cout<<"Ingrese el primer número \n";
+    std::cin>> num1;
+    std::cout<<"Ingrese el segundo número \n";
+    std::cin>> num2;
+    resultado = num1 * num2;
+    std::cout<<resultado<<"\n";
+    return 0;
+}
